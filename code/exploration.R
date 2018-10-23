260ccc69b95d57a0a395ad2bab7584944477acf4
Fernando Cagua <efcaguab@gmail.com>
2018-10-23 21:22:30 +1300

    Create function to extract first word of a string
    Which is used to get the genus from a species name

--- code/exploration.R
+++ code/exploration.R
@@ -12,9 +12,16 @@ get_species_list <- function(networks, metadata){
     dplyr::mutate(guild = "pol")
   
   dplyr::bind_rows(plants, pollinators) %>%
-    dplyr::mutate(genus = gsub(pattern = "\\s.+", 
-                               replacement = "", 
-                               x = sp_name)) %>%
+    dplyr::mutate(genus = get_first_word(sp_name)) %>%
+    dplyr::inner_join(locality_info, by = "net_name")
+}
+
+# return the first word of the string (which is assumed to be the genus in species name)
+get_first_word <- function(x){
+  gsub(pattern = "\\s.+", 
+       replacement = "", 
+       x = x)
+}
     dplyr::inner_join(locality_info, by = "net_name")
 }
 
