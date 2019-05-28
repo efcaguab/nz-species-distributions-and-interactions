2efc3948ea41f0e060cd2d012af5965a573d25d2
Fernando Cagua <efcaguab@gmail.com>
2019-05-28 18:50:07 +1200

    Remove data exploration files

--- main.R
+++ main.R
@@ -58,7 +58,6 @@ download_ocurrence_data_plan <- drake_plan(
   spp_to_download = select_species_to_download(spp, minimum_spp_locations)
 )
 
-
 # Referencing -------------------------------------------------------------
 
 data_references_plan <- drake_plan(
@@ -67,7 +66,6 @@ data_references_plan <- drake_plan(
                               file_out("data/int_data_references.csv"))
 )
 
-
 # Figures -----------------------------------------------------------------
 
 figures_plan <- drake_plan(
@@ -75,60 +73,9 @@ figures_plan <- drake_plan(
   fig_worldmap = plot_worldmap(world_land, int_metadata)
 )
 
-# exploration------
-
-read_data_plan <- drake_plan(
-  networks = read_networks(network_folder = "./data/raw/web-of-life_2018-10-23_042854"),
-  metadata = read_metadata(file_in("./data/raw/web-of-life_2018-10-23_042854/references.csv"))
-)
-
-exploration_plan <- drake_plan(
-  species_list = get_species_list(networks, metadata),
-  interaction_list = get_interaction_list(networks, metadata),
-  # the thresholds are specificied on the number of locations a species is
-  focal_species_options = get_focal_species_options(species_list, interaction_list, categ = "loc"),
-  sp_list_extra_info = get_sp_extra_info(focal_species_options, 10),
-  backbones = get_sp_backbones(sp_list_extra_info),
-  n_occurrences = get_n_ocurrences(backbones),
-  maps = fetch_maps(backbones),
-  turnover_exploration = my_render(knitr_in("./paper/data-exploration/turnover.Rmd"),
-            file_out("./paper/data-exploration/turnover.md"))
-)
-
-
-reporting_plan <- drake_plan(
-  bibliography = target(
-    command = get_bibliography("https://raw.githubusercontent.com/efcaguab/phd-bibliography/master/interactions%2Bsdm_manuscript.bib",
-                               file_out("paper/bibliography.bib"))
-  ),
-  interaction_bibliography = target(
-    command = get_bibliography("https://raw.githubusercontent.com/efcaguab/phd-bibliography/master/interactions%2Bsdm_interaction-data-references.bib", 
-                               file_out("paper/int-bibliography.bib"))
-  ),
-  interaction_citations = bib2df::bib2df(file_in("paper/int-bibliography.bib")),
-  abstract = readLines(file_in("./paper/abstract.md")),
-  keywords = process_keywords(file_in("./paper/keywords.md")),
-  acknowledgements = readLines(file_in("./paper/acknowledgements.md")),
-  intro_line_number = get_line_number(file_in("paper/manuscript.Rmd"), "# Introduction"),
-  abs_wordcount = count_words(file_in("paper/abstract.md")),
-  msc_wordcount = count_words(file_in('paper/manuscript.Rmd'), lines_to_ignore = 1:intro_line_number),
-  n_references = count_references(file_in('paper/manuscript.Rmd'), lines_to_ignore = 1:intro_line_number, refs_to_exclude = "@ref"),
-  n_displays = count_displays(file_in('paper/manuscript.Rmd'), lines_to_ignore = 1:intro_line_number),
-  msc_title = get_yaml_title(file_in('paper/manuscript.Rmd')),
-  supp_info = render_pdf(knitr_in('paper/supp-info.Rmd'), file_out('paper/supp-info.pdf'), clean_md = FALSE),
-  draft_info = render_pdf(file_in('paper/draft-info.Rmd'), file_out('paper/draft-info.pdf'), clean_md = FALSE),
-  manuscript = render_pdf(knitr_in('paper/manuscript.Rmd'), file_out('paper/manuscript.pdf'), clean_md = FALSE),
-  cover_letter = knitr::knit2pdf(knitr_in("paper/cover-letter.Rnw"), output = file_out("paper/cover-letter.tex"))
-)
-
-full_plan <- rbind(
-  read_data_plan,
-  exploration_plan,
-  reporting_plan
-)
 
 paper_plan <- rbind(
-  full_plan,
+  # full_plan,
   configuration_plan,
   get_data_plan,
   pre_process_int_plan,
