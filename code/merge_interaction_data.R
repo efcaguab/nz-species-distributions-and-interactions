28ccb02d36f2ccb245271b5dc7105b02b324dfc2
Fernando Cagua <efcaguab@gmail.com>
2019-06-11 18:28:45 +1200

    Spp target should return and ungrouped data frame

--- code/merge_interaction_data.R
+++ code/merge_interaction_data.R
@@ -5,7 +5,10 @@ merge_spp <- function(wol_spp){
     mutate(sp_unidentified = stringr::str_detect(sp_name, "M_PL"), 
            gen_unidentified = genus %in% c("Unientified", "Undefined", "Unidentified")) %>%
     group_by(loc_id) %>%
-    mutate(node_id = paste0(loc_id, "-", guild, "_",stringr::str_pad(1:n(), width = 4, pad = "0")))
+    mutate(node_id = paste0(loc_id, "-", 
+                            guild, "_",
+                            stringr::str_pad(1:n(), width = 4, pad = "0"))) %>%
+    ungroup()
 }
 
 # merge interactions from multiple interaction data sources
