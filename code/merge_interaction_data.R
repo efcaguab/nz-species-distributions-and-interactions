f36ec92ef5408aca7716e030a630bd2df90128e6
Fernando Cagua <efcaguab@gmail.com>
2019-06-11 12:44:11 +1200

    Create an unique sp index per site

--- code/merge_interaction_data.R
+++ code/merge_interaction_data.R
@@ -1,8 +1,11 @@
 # merge species list from multiple interaction data sources
 merge_spp <- function(wol_spp){
+  require(dplyr)
   wol_spp %>%
-    dplyr::mutate(sp_unidentified = stringr::str_detect(sp_name, "M_PL"), 
-                  gen_unidentified = genus %in% c("Unientified", "Undefined", "Unidentified"))
+    mutate(sp_unidentified = stringr::str_detect(sp_name, "M_PL"), 
+           gen_unidentified = genus %in% c("Unientified", "Undefined", "Unidentified")) %>%
+    group_by(loc_id) %>%
+    mutate(node_id = paste0(loc_id, "-", guild, "_",stringr::str_pad(1:n(), width = 4, pad = "0")))
 }
 
 # merge interactions from multiple interaction data sources
