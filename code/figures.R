975a527f37dbaa7ceb2bd3b0a35672d7a8538b48
Fernando Cagua <efcaguab@gmail.com>
2019-06-18 16:29:18 +1200

    Only show locations with valid interactions in map

--- code/figures.R
+++ code/figures.R
@@ -1,10 +1,15 @@
-plot_worldmap <- function(world_land, int_metadata){
+plot_worldmap <- function(world_land, int_metadata, clean_interactions){
   require(ggplot2)
+  
+  valid_locations <- unique(clean_interactions$loc_id)
+  
+  location_data <- int_metadata %>%
+    dplyr::filter(loc_id %in% valid_locations)
   
   p <- ggplot(world_land) +
     geom_sf(fill = cgm()$pal_rb3[2], colour = "antiquewhite4", 
             lwd = 0.25) +
-    geom_point(data = int_metadata,
+    geom_point(data = location_data,
                aes(x = lon, y = lat),
                shape = 21,
                fill = "white", 
