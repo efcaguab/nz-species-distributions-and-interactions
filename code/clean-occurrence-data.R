ba5f06bb347219a3a6eda763f869457b086f73dc
Fernando Cagua <efcaguab@gmail.com>
2019-06-25 17:33:23 +1200

    Finish off function to clean coordinate data

--- code/clean-occurrence-data.R
+++ code/clean-occurrence-data.R
@@ -1,34 +1,50 @@
-clean_occurrences <- function(dirty_occurrence_df){
-  dirty_occurrence_df <- readr::read_csv("data/downloads/spp_ocurrences/Adia cinerella.csv")
+clean_occurrences <- function(dirty_occurrence_df, land_data, country_data_sf){
+  # dirty_occurrence_df <- readr::read_csv("data/downloads/spp_ocurrences/Adia cinerella.csv")
   occurrence_df <- dirty_occurrence_df %>%
-    dplyr::mutate(sp_id = "a") %>% # only for testing
+    # dplyr::mutate(sp_id = "a") %>% # only for testing
     dplyr::mutate(decimallongitude = as.numeric(as.character(decimalLongitude)), 
                   decimallatitude = as.numeric(as.character(decimalLatitude)), 
-                  coordinateUncertaintyInMeters  = as.numeric(as.character(coordinateUncertaintyInMeters)), 
+                  coordinateUncertaintyInMeters  = 
+                    as.numeric(as.character(coordinateUncertaintyInMeters)), 
                   year  = as.numeric(as.character(year)), 
-                  country_code_iso3c = countrycode::countrycode(countryCode, origin =  'iso2c', destination = 'iso3c')) %>%
+                  country_code_iso3c = 
+                    countrycode::countrycode(countryCode, 
+                                             origin =  'iso2c', 
+                                             destination = 'iso3c')) %>%
     dplyr::filter(!is.na(decimalLongitude),
                   !is.na(decimalLatitude),
-                  !is.na(countryCode))# %>% 
-    # dplyr::mutate(valid_coordinate = 
-                    # CoordinateCleaner::cc_val(x = ., 
-                                              # lon = "decimalLongitude",
-                                              # lat = "decimalLatitude",
-                                              # value = "flagged"))
-  
-  cleaned_occurrences <- CoordinateCleaner::clean_coordinates(occurrence_df, 
-                                       species = "sp_id", 
-                                       countries = "country_code_iso3c",
-                                       seas_ref = land_data, 
-                                       seas_scale = 10,
-                                       verbose = FALSE) 
-  
-  library(ggplot2)
-  cleaned_occurrences %>%
-    ggplot(aes(x = decimallongitude, y = decimallatitude)) +
-    geom_map(map = map_data('world'),
-             data = map_data('world'),
-             aes(map_id = region, x = long, y = lat, group = group),
-             fill = "white") +
-    geom_point(aes(colour = .sea, shape = .otl))
+                  !is.na(countryCode),
+                  coordinateUncertaintyInMeters/1000 <= 100 |
+                    is.na(coordinateUncertaintyInMeters),
+                  basisOfRecord == "HUMAN_OBSERVATION" |
+                    basisOfRecord == "OBSERVATION" |
+                    basisOfRecord == "PRESERVED_SPECIMEN",
+                  individualCount > 0 | is.na(individualCount),
+                  individualCount < 99 | is.na(individualCount),
+                  year > 1945)
+    
+  cleaned_occurrences <- occurrence_df %>%
+    dplyr::mutate(.sea_manual = CoordinateCleaner::cc_sea(., ref = land_data,
+                                                   scale = 10, 
+                                                   value = "flagged", 
+                                                   verbose = FALSE)) %>%
+    
+    CoordinateCleaner::clean_coordinates(tests = c("capitals",
+                                                   "centroids", 
+                                                   "equal", 
+                                                   "gbif", 
+                                                   "institutions", 
+                                                   "zeros"),
+                                         species = "sp_name", 
+                                         countries = "country_code_iso3c",
+                                         verbose = FALSE)
+  # library(ggplot2)
+  # cleaned_occurrences %>%
+  #   ggplot(aes(x = decimallongitude, y = decimallatitude)) +
+  #   geom_map(map = map_data('world'),
+  #            data = map_data('world'),
+  #            aes(map_id = region, x = long, y = lat, group = group),
+  #            fill = "white") +
+  #   geom_point(aes(colour = .sea, shape = .otl))
 }
+
