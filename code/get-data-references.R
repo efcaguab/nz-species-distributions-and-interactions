7608977f152179dee9fce1637d56c14b4f03d618
Fernando Cagua <efcaguab@gmail.com>
2019-07-02 16:17:13 +1200

    When the refereces for data sources have been downloaded already dont do it again
    Close #15

--- code/get-data-references.R
+++ code/get-data-references.R
@@ -1,22 +1,29 @@
-get_int_ref_dois <- function(int_metadata){
-  require(dplyr)
-  
-  refs <- int_metadata %>%
-    dplyr::mutate(published = is_reference_published(reference)) %>%
-    dplyr::filter(published) %$%
-    reference %>%
-    unique() 
-  
-  dois <- refs %>% 
-    purrr::map_df(~ rcrossref::cr_works(query = ., 
-                                     limit = 1, 
-                                     sort = "score", 
-                                     select = "DOI")$data)
-  
-  dois %>%
-    mutate(reference = refs) %>%
-    rename(likely_doi = doi)
-  
+# Get references of data sources
+get_int_ref_dois <- function(int_metadata, out_file){
+  suppressPackageStartupMessages({
+    require(dplyr)
+  })
+  # If there no previous file storing that info download it
+  if (!file.exists(out_file)) {
+    refs <- int_metadata %>%
+      dplyr::mutate(published = is_reference_published(reference)) %>%
+      dplyr::filter(published) %$%
+      reference %>%
+      unique() 
+    
+    dois <- refs %>% 
+      purrr::map_df(~ rcrossref::cr_works(query = ., 
+                                          limit = 1, 
+                                          sort = "score", 
+                                          select = "DOI")$data)
+    
+    interaction_references <- dois %>%
+      mutate(reference = refs) %>%
+      rename(likely_doi = doi)
+    
+    readr::write_csv(interaction_references, out_file)
+  }
+  readr::read_csv(out_file, col_types = "cc")
 }
 
 # determine if a reference (string) is likely published
