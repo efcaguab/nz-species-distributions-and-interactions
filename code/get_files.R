85d4cd2d604807064f0d0b074dc512f8d09ef089
Fernando Cagua <efcaguab@gmail.com>
2019-07-02 14:58:15 +1200

    If file is there don't download
    Issue #15

--- code/get_files.R
+++ code/get_files.R
@@ -1,6 +1,12 @@
 
-# DOwnload itis synonym database. As a zip file
+# Download itis synonym database. As a zip file
 get_file <- function(itis_address, dest_file, download_date = NULL){
-  message("donwloading data on ", download_date)
-  download.file(itis_address, dest_file)
+  if(!file.exists(dest_file)){
+    message("donwloading data on ", download_date)
+    download.file(itis_address, dest_file)
+  } else {
+    message("file already found")
+    return(0)
+  }
+  
 }
