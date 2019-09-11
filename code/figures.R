16d37327ce0dd8b167d26df9e43b30ce1f095846
efcaguab <efcaguab@gmail.com>
2019-09-12 09:48:45 +1200

    Remove trailing whitespace from a couple more script files

--- code/figures.R
+++ code/figures.R
@@ -1,126 +1,126 @@
 plot_worldmap <- function(world_land, int_metadata, clean_interactions){
   require(ggplot2)
-  
+
   valid_locations <- unique(clean_interactions$loc_id)
-  
+
   location_data <- int_metadata %>%
     dplyr::filter(loc_id %in% valid_locations)
-  
+
   p <- ggplot(world_land) +
-    geom_sf(fill = cgm()$pal_rb3[2], colour = "antiquewhite4", 
+    geom_sf(fill = cgm()$pal_rb3[2], colour = "antiquewhite4",
             lwd = 0.25) +
     geom_point(data = location_data,
                aes(x = lon, y = lat),
                shape = 21,
-               fill = "white", 
-               size = 1, 
+               fill = "white",
+               size = 1,
                color = cgm()$pal_el_green[9]) +
     coord_sf(expand = FALSE) +
     pub_theme() +
-    theme(panel.grid = element_line(linetype = "dashed", colour = gray(0.5), size = 0.25), 
+    theme(panel.grid = element_line(linetype = "dashed", colour = gray(0.5), size = 0.25),
           axis.title = element_blank()) +
     labs(title = "Geographic location of pollination communities")
-  
+
   p
 }
 
 # plot distribution of number of species per number of locations
 plot_species_location_distribution <- function(dist_species_multiple_locations_data){
-  
+
   suppressPackageStartupMessages({
     require(ggplot2)
     require(ggforce)
   })
-  
+
   pal <- cgm()$pal_el_green[c(4,7)]
-  
+
    p <- dist_species_multiple_locations_data %>%
     ggplot(aes(x = n_locations, y = n_species, fill = guild)) +
     geom_line(aes(colour = guild), linetype = 3, size = 0.5) +
     geom_point(shape = 21, size = 1) +
     # geom_text_repel(data = distribution_data_highlight, aes(label = sp_name)) +
-    geom_mark_circle(aes(label = sp_name, filter = !is.na(sp_name), group = sp_name), 
-                     expand = unit(0.1, "mm"), 
+    geom_mark_circle(aes(label = sp_name, filter = !is.na(sp_name), group = sp_name),
+                     expand = unit(0.1, "mm"),
                      alpha = 0.1,
-                     colour = NA, 
-                     label.fontsize = 7, 
+                     colour = NA,
+                     label.fontsize = 7,
                      label.buffer = unit(5, "mm"),
                      label.minwidth = unit(10, "mm"),
                      label.margin = margin(0.5, 0.5, 0.5, 0.5, "mm"),
-                     label.fontface = "italic", 
-                     con.size = 0.25, 
-                     con.cap = unit(1, "mm"), 
+                     label.fontface = "italic",
+                     con.size = 0.25,
+                     con.cap = unit(1, "mm"),
                      con.type = "straight") +
-    scale_fill_manual(values = pal, aesthetics = c("fill", "colour"), 
+    scale_fill_manual(values = pal, aesthetics = c("fill", "colour"),
                       labels = c("animals", "plants")) +
     scale_y_log10(labels = scales::number_format(big.mark = ",")) +
     scale_x_log10(breaks = c(1,2, 4, 8, 16, 32)) +
     pub_theme() +
-    theme(legend.position = c(0.95,0.95), 
-          legend.justification = c(1,1), 
+    theme(legend.position = c(0.95,0.95),
+          legend.justification = c(1,1),
           legend.title = element_blank()) +
-    labs(x = "number of locations", 
+    labs(x = "number of locations",
          y = "frequency (# species)",
-         title = "number of species at multiple locations", 
+         title = "number of species at multiple locations",
          subtitle = "frequency distribution")
-    
+
   # ggsave("plot.pdf", p,  width = unit(width("single"), "in"), height = unit(2.2, "in"))
    p
 }
 
 
 get_dist_species_multiple_locations_data <- function(clean_interactions, checked_sp_names, species_ids){
-  
+
   distribution_data <- clean_interactions %>%
     tidyr::gather("guild", "sp_id", pla_id, ani_id) %>%
     dplyr::group_by(guild, sp_id) %>%
     dplyr::summarise(n_locations = dplyr::n_distinct(loc_id)) %>%
     dplyr::group_by(guild, n_locations) %>%
-    dplyr::summarise(n_species = dplyr::n_distinct(sp_id), 
+    dplyr::summarise(n_species = dplyr::n_distinct(sp_id),
                      sp_id = paste(sp_id, collapse = "."))
-  
+
   sp_id_to_highlight <- distribution_data %>%
     dplyr::group_by(guild) %>%
-    dplyr::filter(n_species == 1, 
+    dplyr::filter(n_species == 1,
                   n_locations == max(n_locations)
     ) %$%
     sp_id
-  
+
   species_to_highlight <- species_ids %>%
     dplyr::filter(sp_id %in% sp_id_to_highlight)
-  
+
   distribution_data_highlight <- distribution_data %>%
     dplyr::inner_join(species_to_highlight, by = "sp_id") %>%
     dplyr::inner_join(checked_sp_names, by = "sp_name") %>%
     dplyr::filter(gnr_score >= 0.98)
-  
+
   distribution_data %>%
-    dplyr::left_join(distribution_data_highlight, 
-                     by = c("guild", "n_locations", "n_species", "sp_id")) 
-  
+    dplyr::left_join(distribution_data_highlight,
+                     by = c("guild", "n_locations", "n_species", "sp_id"))
+
 }
 
-plot_sensitivity_analysis <- function(error_subsamples, min_suitability_error, 
-                                      min_occurrences_factor, 
+plot_sensitivity_analysis <- function(error_subsamples, min_suitability_error,
+                                      min_occurrences_factor,
                                       suitability_subsamples){
   suppressPackageStartupMessages({
     library(ggplot2)
   })
-  
-  thresholds <- min_occurrences_factor %>% 
-    as.data.frame() %>% 
+
+  thresholds <- min_occurrences_factor %>%
+    as.data.frame() %>%
     tidyr::gather(key = "niche_space") %>%
     dplyr::filter(niche_space == "all_species")
-  
+
   pal <- cgm()$pal_el_green[c(4,7)]
-  
+
   n_nets <- suitability_subsamples$n_net_occurrences[1]
-  
+
   e <- error_subsamples %>%
     dplyr::group_by(niche_space) %>%
     dplyr::mutate(error = mae) %>%
     dplyr::filter(niche_space == "all_species")
-  
+
   p <- e %>%
     ggplot(aes(x = n_occ, y = error)) +
     geom_point(data = dplyr::sample_n(e, 1000),
@@ -131,77 +131,77 @@ plot_sensitivity_analysis <- function(error_subsamples, min_suitability_error,
                 formula = y ~ s(x), se = F,
                 size = 0.5, colour = "black") +
     geom_hline(yintercept = min_suitability_error, size = 0.25, linetype = 2) +
-    geom_vline(data = thresholds, 
+    geom_vline(data = thresholds,
                aes(xintercept = value*n_nets), size = 0.25, linetype = 2) +
-    geom_text(data = thresholds, 
-              aes(label = paste("~",  value, "occurrences\nper community"), 
-                  x = value*n_nets), 
-              y = max(e$error), angle = 90, hjust = 1, vjust = 1.2, 
+    geom_text(data = thresholds,
+              aes(label = paste("~",  value, "occurrences\nper community"),
+                  x = value*n_nets),
+              y = max(e$error), angle = 90, hjust = 1, vjust = 1.2,
               size = 2.3, lineheight = 0.8) +
-    annotate("text", label = "10% error", x = max(e$n_occ), y = 0.1, hjust = 1, 
+    annotate("text", label = "10% error", x = max(e$n_occ), y = 0.1, hjust = 1,
              vjust = -1, size = 2.3) +
     # scale_x_continuous(limits = c(2,35)) +
     scale_x_log10() +
-    # scale_fill_manual(values = pal, aesthetics = c("fill", "colour"), 
-                      # labels = c(" env. space based on all spp. occurrences", 
+    # scale_fill_manual(values = pal, aesthetics = c("fill", "colour"),
+                      # labels = c(" env. space based on all spp. occurrences",
                                  # " env. space based on each spp. occurrences")) +
     pub_theme() +
-    theme(legend.position = "none", 
-          legend.justification = c(1,1), 
+    theme(legend.position = "none",
+          legend.justification = c(1,1),
           legend.title = element_blank()) +
-    labs(x = "number of GBIF occurrences", 
+    labs(x = "number of GBIF occurrences",
          y = "mean absolute error",
-         title = "error of environmental suitability of communities", 
+         title = "error of environmental suitability of communities",
          subtitle = "for a species present in two plant-pollinator communities ")
-  
+
   # ggsave("plot.pdf", p,  width = unit(width("single"), "in"), height = unit(2.2, "in"))
-  
+
   p
 }
 
 plot_all_conditional_effect <- function(cond_draws, mean_parameter_values){
-  
+
   suppressPackageStartupMessages({
     require(ggplot2)
   })
-  
+
   pal <- cgm()$pal_el_green[c(8,7)]
-  
+
   grinell_niche_size_plot <- cond_draws$grinell_niche_size %>%
     plot_conditional_effect_guild(pal, mean_parameter_values$niche_size) +
-    labs(title = "(a) environmental niche size", 
+    labs(title = "(a) environmental niche size",
          x = "environmental niche size (scaled)")
-  
+
   suitability_plot <- cond_draws$suitability %>%
     plot_conditional_effect_guild(pal, mean_parameter_values$suitability) +
-    labs(title = "(b) environmental suitability", 
+    labs(title = "(b) environmental suitability",
          x = "environmental suitability")
-  
+
   generality_plot <- cond_draws$generality %>%
     plot_conditional_effect_guild(pal, mean_parameter_values$generality, TRUE) +
-    labs(title = "(c) generality", 
+    labs(title = "(c) generality",
          x = "# partners across communities")
-  
+
   possible_plot <- cond_draws$possible %>%
     plot_conditional_effect_guild(pal, mean_parameter_values$possible) +
-    labs(title = "(d) possible number of interactions", 
+    labs(title = "(d) possible number of interactions",
          x = "# possible interactions")
-  
-  p <- cowplot::plot_grid(grinell_niche_size_plot, 
-                          suitability_plot, 
+
+  p <- cowplot::plot_grid(grinell_niche_size_plot,
+                          suitability_plot,
                           generality_plot,
                           possible_plot,
-                          ncol = 1, 
+                          ncol = 1,
                           align = "hv", axis = "lt")
   p
   # ggsave("plot.pdf", p,  width = unit(width("single"), "in"), height = unit(2.2*3, "in"))
-  
+
 }
 
 plot_conditional_effect_guild <- function(data, pal, mean_val, log_transformed = FALSE){
-  
+
   if(log_transformed) mean_val <- exp(mean_val)
-  
+
    data %>%
     dplyr::ungroup() %>%
     dplyr::mutate(guild = translate_guild(guild, "effect")) %>%
@@ -210,7 +210,7 @@ plot_conditional_effect_guild <- function(data, pal, mean_val, log_transformed =
     geom_line(alpha = 0.15, size = 0.25) +
     geom_line(aes(group = guild), stat = "summary",fun.y = "mean", size = 1) +
     facet_wrap(~guild) +
-    scale_fill_manual(values = pal, aesthetics = c("fill", "colour"), 
+    scale_fill_manual(values = pal, aesthetics = c("fill", "colour"),
                       labels = c(" env. space based on all spp. occurrences",
                                  " env. space based on each spp. occurrences")) +
     pub_theme() +
@@ -220,54 +220,54 @@ plot_conditional_effect_guild <- function(data, pal, mean_val, log_transformed =
 }
 
 plot_ranf <- function(random_species_draws, random_correlation_posterior, random_sp_names){
-  
+
   suppressPackageStartupMessages({
     require(ggplot2)
     require(tidybayes)
     require(ggforce)
   })
-  
+
   pal <- cgm()$pal_el_green[c(8,7)]
-  
+
   conditional_effects_plot <- random_species_draws %>%
     dplyr::ungroup() %>%
     dplyr::mutate(guild = translate_guild(guild)) %>%
     dplyr::left_join(random_sp_names, by = "org_id") %>%
     ggplot(aes(x = var, y = .value, colour = guild)) +
     geom_line(aes(group = org_id, alpha = highlight, size = highlight), stat = "summary",fun.y = "mean") +
-    geom_mark_circle(aes(group = org_id, 
-                         filter = highlight & mark, 
-                          label = sp_name), 
-                     expand = unit(0, "mm"), 
+    geom_mark_circle(aes(group = org_id,
+                         filter = highlight & mark,
+                          label = sp_name),
+                     expand = unit(0, "mm"),
                      alpha = 0.1,
-                      # concavity = 0, 
-                      colour = "transparent", 
-                      fill = "grey", 
-                      label.fontsize = 7, 
+                      # concavity = 0,
+                      colour = "transparent",
+                      fill = "grey",
+                      label.fontsize = 7,
                       label.buffer = unit(5, "mm"),
                       label.minwidth = unit(10, "mm"),
                       label.margin = margin(0.5, 0.5, 0.5, 0.5, "mm"),
-                      label.fontface = "italic", 
-                      con.size = 0.25, 
-                      con.cap = unit(0.1, "mm"), 
+                      label.fontface = "italic",
+                      con.size = 0.25,
+                      con.cap = unit(0.1, "mm"),
                       con.type = "straight") +
     facet_wrap(~guild, ncol = 1) +
     scale_alpha_manual(values = c(0.5, 1)) +
     scale_size_manual(values = c(0.25, 0.5)) +
-    scale_fill_manual(values = pal, aesthetics = c("fill", "colour"), 
+    scale_fill_manual(values = pal, aesthetics = c("fill", "colour"),
                       labels = c(" env. space based on all spp. occurrences",
                                  " env. space based on each spp. occurrences")) +
     pub_theme() +
     coord_cartesian(expand = F) +
     theme(legend.position = "none") +
-    labs(y = "# interactions", 
+    labs(y = "# interactions",
          x = "environmental suitability",
          title = "(a) effect of suitability on individual species")
 
   mean_correlation <- random_correlation_posterior %>%
-    dplyr::summarise_all(mean) %$% 
+    dplyr::summarise_all(mean) %$%
     correlation
-  
+
   correlation_plot <- random_correlation_posterior %>%
     ggplot(aes(x = correlation)) +
     geom_density(fill = cgm()$pal_el_green[1], colour = NA) +
@@ -275,19 +275,19 @@ plot_ranf <- function(random_species_draws, random_correlation_posterior, random
     geom_vline(xintercept = mean_correlation, size = 0.25, linetype = 2) +
     coord_cartesian(expand = T) +
     pub_theme() +
-    theme(panel.border = element_blank(), 
-          axis.title.y = element_blank(), 
-          axis.text.y = element_blank(), 
-          axis.ticks.y = element_blank(), 
+    theme(panel.border = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
           axis.line.x.bottom = element_line()) +
     labs(title = "(b) correlation between random intercept and slope")
 
-  
-  p <- cowplot::plot_grid(conditional_effects_plot, correlation_plot, 
+
+  p <- cowplot::plot_grid(conditional_effects_plot, correlation_plot,
                           ncol = 1 ,
-                          rel_heights = c(2,0.5), 
+                          rel_heights = c(2,0.5),
                           align = "v")
   # ggsave("plot.pdf", p, width = unit(width("single"), "in"), height = unit(2.2*2.5, "in"))
-  
-  p  
+
+  p
 }
--- code/model_predictions.R
+++ code/model_predictions.R
@@ -1,6 +1,6 @@
 
 get_scale_attributes <- function(x){
-  list(scale = attr(x, 'scaled:scale'), 
+  list(scale = attr(x, 'scaled:scale'),
        center = attr(x, 'scaled:center'))
 }
 
@@ -13,20 +13,20 @@ scaled_to_unscaled <- function(x, scale_attr){
 }
 
 get_parameter_posterior_summaries <- function(this_model){
-  brms::posterior_summary(this_model) %>% 
+  brms::posterior_summary(this_model) %>%
     as.data.frame() %>%
-    tibble::rownames_to_column(var = "parameter") 
+    tibble::rownames_to_column(var = "parameter")
 }
 
 get_median_number_trials <- function(model){
   model$data %>%
     dplyr::distinct(guild, n_opposite_guild) %>%
-    dplyr::group_by(guild) %>% 
+    dplyr::group_by(guild) %>%
     dplyr::summarise(n_opposite_guild = median(n_opposite_guild))
 }
 
 draw_conditional_fits <- function(this_model, median_trials, scale_attr){
-  
+
   suppressPackageStartupMessages({
     require(brms)
     require(tidybayes)
@@ -39,111 +39,111 @@ draw_conditional_fits <- function(this_model, median_trials, scale_attr){
   #   this_model <- chosen_models$formula_base
 
   grinell_niche_size <- median_trials %>%
-    tidyr::crossing(scaled_grinell_niche_size = seq(-2, 2, length.out = 10), 
-                    scaled_suitability = 0, 
-                    scaled_log_n_partners_global = 0, 
-                    scaled_n_possible_partners = 0) %>%
-    add_fitted_draws(this_model, re_formula = NA, n = 100) %>% 
-    dplyr::mutate(var = scaled_grinell_niche_size) 
-  
-  suitability <- median_trials %>%
-    tidyr::crossing(scaled_grinell_niche_size = 0, 
-                    scaled_suitability = seq(unscaled_to_scaled(0, scale_attr$suitability),
-                                             unscaled_to_scaled(1, scale_attr$suitability), length.out = 10), 
-                    scaled_log_n_partners_global = 0, 
+    tidyr::crossing(scaled_grinell_niche_size = seq(-2, 2, length.out = 10),
+                    scaled_suitability = 0,
+                    scaled_log_n_partners_global = 0,
                     scaled_n_possible_partners = 0) %>%
     add_fitted_draws(this_model, re_formula = NA, n = 100) %>%
-    dplyr::mutate(suitability = scaled_to_unscaled(scaled_suitability, scale_attr$suitability), 
-                  var = suitability) 
+    dplyr::mutate(var = scaled_grinell_niche_size)
+
+  suitability <- median_trials %>%
+    tidyr::crossing(scaled_grinell_niche_size = 0,
+                    scaled_suitability = seq(unscaled_to_scaled(0, scale_attr$suitability),
+                                             unscaled_to_scaled(1, scale_attr$suitability), length.out = 10),
+                    scaled_log_n_partners_global = 0,
+                    scaled_n_possible_partners = 0) %>%
+    add_fitted_draws(this_model, re_formula = NA, n = 100) %>%
+    dplyr::mutate(suitability = scaled_to_unscaled(scaled_suitability, scale_attr$suitability),
+                  var = suitability)
 
   generality <- median_trials %>%
-    tidyr::crossing(scaled_grinell_niche_size = 0, 
-                    scaled_suitability = 0, 
+    tidyr::crossing(scaled_grinell_niche_size = 0,
+                    scaled_suitability = 0,
                     scaled_log_n_partners_global = seq(unscaled_to_scaled(log(1), scale_attr$generality),
-                                                       2, #unscaled_to_scaled(2, scale_attr$generality), 
-                                                       length.out = 20), 
+                                                       2, #unscaled_to_scaled(2, scale_attr$generality),
+                                                       length.out = 20),
                     scaled_n_possible_partners = 0) %>%
     add_fitted_draws(this_model, re_formula = NA, n = 100) %>%
-    dplyr::mutate(log_n_partners_global = scaled_to_unscaled(scaled_log_n_partners_global, scale_attr$generality), 
+    dplyr::mutate(log_n_partners_global = scaled_to_unscaled(scaled_log_n_partners_global, scale_attr$generality),
                   n_partners_global = exp(log_n_partners_global),
                   var = n_partners_global) %>%
-    dplyr::filter(guild == "ani_id") 
-  
+    dplyr::filter(guild == "ani_id")
+
   possible <- median_trials %>%
-    tidyr::crossing(scaled_grinell_niche_size = 0, 
-                    scaled_suitability = 0, 
-                    scaled_log_n_partners_global = 0, 
+    tidyr::crossing(scaled_grinell_niche_size = 0,
+                    scaled_suitability = 0,
+                    scaled_log_n_partners_global = 0,
                     scaled_n_possible_partners = seq(unscaled_to_scaled(1, scale_attr$possible),
-                                                     2, 
+                                                     2,
                                                      length.out = 20)) %>%
     add_fitted_draws(this_model, re_formula = NA, n = 100) %>%
-    dplyr::mutate(n_possible_partners = scaled_to_unscaled(scaled_n_possible_partners, scale_attr$possible), 
+    dplyr::mutate(n_possible_partners = scaled_to_unscaled(scaled_n_possible_partners, scale_attr$possible),
                   var = n_possible_partners ) %>%
-    dplyr::filter(guild == "ani_id") 
-  
-  list(grinell_niche_size = grinell_niche_size, 
-       suitability = suitability, 
-       generality = generality, 
+    dplyr::filter(guild == "ani_id")
+
+  list(grinell_niche_size = grinell_niche_size,
+       suitability = suitability,
+       generality = generality,
        possible = possible)
 }
 
 get_all_pars_scale_attributes <- function(this_model){
   list(
     niche_size = list(scale = 1, center = 0),
-    suitability = get_scale_attributes(this_model$data$scaled_suitability), 
-    generality = get_scale_attributes(this_model$data$scaled_log_n_partners_global), 
+    suitability = get_scale_attributes(this_model$data$scaled_suitability),
+    generality = get_scale_attributes(this_model$data$scaled_log_n_partners_global),
     possible = get_scale_attributes(this_model$data$scaled_n_possible_partners)
   )
 }
 
 draw_conditional_random_species <- function(this_model, median_trials, scale_attr){
-  
+
   sp_properties <- this_model$data %>%
     dplyr::group_by(org_id) %>%
     dplyr::mutate(n_obs = dplyr::n_distinct(loc_id)) %>%
     dplyr::distinct(guild, org_id, n_obs, scaled_log_n_partners_global,scaled_grinell_niche_size) %>%
     dplyr::ungroup() %>%
     dplyr::filter(n_obs >= 6)
-  
+
   slopes <- brms::posterior_samples(this_model) %>%
     dplyr::select(dplyr::contains("r_org_id")) %>%
-    dplyr::select(dplyr::contains("scaled_suitability")) %>% 
-    dplyr::select(-dplyr::contains("cor")) %>% 
+    dplyr::select(dplyr::contains("scaled_suitability")) %>%
+    dplyr::select(-dplyr::contains("cor")) %>%
     tidyr::gather(key = "col", value = "value") %>%
     dplyr::mutate(org_id = substr(col, 10, 18)) %>%
     dplyr::inner_join(sp_properties, by = "org_id") %>%
     dplyr::group_by(guild, org_id) %>%
     dplyr::summarise(value = mean(value)) %>%
-    dplyr::group_by(guild) 
-  
+    dplyr::group_by(guild)
+
   sp_to_highlight <- slopes %>%
     dplyr::filter(value == max(value) | value == min(value)) %$% org_id
-  
+
   draws <- median_trials %>%
     dplyr::inner_join(sp_properties, by = "guild") %>%
     tidyr::crossing(scaled_suitability = seq(unscaled_to_scaled(0, scale_attr$suitability),
-                                             unscaled_to_scaled(1, scale_attr$suitability), length.out = 10), 
+                                             unscaled_to_scaled(1, scale_attr$suitability), length.out = 10),
                     scaled_n_possible_partners = 0) %>%
     tidybayes::add_fitted_draws(this_model, re_formula = ~ (1 + scaled_suitability | org_id), n = 100)
-  
+
   draws %>%
     dplyr::group_by(guild, org_id, scaled_suitability) %>%
     dplyr::summarise(.value = mean(.value)) %>%
-    dplyr::mutate(suitability = scaled_to_unscaled(scaled_suitability, scale_attr$suitability), 
-                  var = suitability, 
+    dplyr::mutate(suitability = scaled_to_unscaled(scaled_suitability, scale_attr$suitability),
+                  var = suitability,
                   highlight = org_id %in% sp_to_highlight) %>%
     dplyr::inner_join(slopes, by = c("guild", "org_id")) %>%
     dplyr::group_by(org_id) %>%
-    dplyr::mutate(suitability_percent = dplyr::percent_rank(suitability), 
-                  s_left = abs(suitability_percent - 0.1) == min(abs(suitability_percent - 0.1)), 
-                  s_right = abs(suitability_percent - 0.9) == min(abs(suitability_percent - 0.9)), 
-                  mark = value > 0 & s_right | value < 0 & s_left) 
-   
+    dplyr::mutate(suitability_percent = dplyr::percent_rank(suitability),
+                  s_left = abs(suitability_percent - 0.1) == min(abs(suitability_percent - 0.1)),
+                  s_right = abs(suitability_percent - 0.9) == min(abs(suitability_percent - 0.9)),
+                  mark = value > 0 & s_right | value < 0 & s_left)
+
 }
 
 get_posterior_random_correlation <- function(baseline_model){
-  
-  brms::posterior_samples(baseline_model, pars = c("cor_org_id__Intercept__scaled_suitability")) %>% 
+
+  brms::posterior_samples(baseline_model, pars = c("cor_org_id__Intercept__scaled_suitability")) %>%
     dplyr::mutate(correlation = cor_org_id__Intercept__scaled_suitability)
 }
 
@@ -151,7 +151,7 @@ get_random_sp_names <- function(random_species_draws, org_ids, gbif_key_groups, 
                                 species_ids){
   random_species_draws %>%
     dplyr::filter(highlight) %$%
-    org_id %>% 
+    org_id %>%
     unique() %>%
     purrr::map(get_sp_name, org_ids, gbif_key_groups, gbif_keys,
                species_ids) %>%
@@ -162,15 +162,15 @@ get_random_sp_names <- function(random_species_draws, org_ids, gbif_key_groups, 
 tinker <- function(){
   # model_data <- standata(this_model)
   # conditions <- data.frame(guild = c("pla_id", "ani_id"))
-  
+
   # me <- marginal_effects(this_model, "scaled_grinell_niche_size", conditions = median_trials)
-  
+
   this_model %>%
-    spread_draws(b_scaled_suitability, 
-                 # b_guildpla_id, 
-                 b_scaled_grinell_niche_size, 
-                 b_scaled_log_n_partners_global, 
-                 b_scaled_n_possible_partners) %>% 
+    spread_draws(b_scaled_suitability,
+                 # b_guildpla_id,
+                 b_scaled_grinell_niche_size,
+                 b_scaled_log_n_partners_global,
+                 b_scaled_n_possible_partners) %>%
     tidyr::gather("variable", "value", dplyr::starts_with("b_")) %>%
     ggplot(aes(x = value, y = variable)) +
     geom_vline(xintercept = 0, size = 0.25, linetype = 2) +
@@ -182,19 +182,19 @@ tinker <- function(){
     pub_theme() +
     theme(legend.position = "none") +
     labs(title = "(b) Effect sizes")
-  
+
   effect <- this_model %>%
-    spread_draws(b_scaled_grinell_niche_size) %>% 
+    spread_draws(b_scaled_grinell_niche_size) %>%
     tidyr::gather("variable", "value", dplyr::starts_with("b_")) %>%
     ggplot(aes(x = value)) +
     stat_density(geom = "line") +
     geom_vline(xintercept = 0, linetype = 2, size = 0.25) +
     facet_wrap(~ variable, scales = "free") +
     pub_theme() +
-    theme(panel.border = element_blank(), 
-          axis.line.x = element_line(), 
-          axis.ticks.y = element_blank(), 
-          axis.text.y = element_blank(), 
+    theme(panel.border = element_blank(),
+          axis.line.x = element_line(),
+          axis.ticks.y = element_blank(),
+          axis.text.y = element_blank(),
           axis.title.y = element_blank()) +
     labs(title = "(b) Effect of environmental niche size")
 }
