acd1e83f4e1a4d3f73d24fcb68909c28803f6d30
Fernando Cagua <efcaguab@gmail.com>
2019-09-11 17:25:39 +1200

    Add number of cores

--- code/suitability_vs_degree.R
+++ code/suitability_vs_degree.R
@@ -100,7 +100,7 @@ build_analysis_frame <- function(org_degree,
 }
 
 # Fit all models in a crossed formulas and data list
-fit_all_models <- function(formulas_and_data, n_markov_iter){
+fit_all_models <- function(formulas_and_data, n_markov_iter, brm_cores){
  purrr::map(formulas_and_data,
             ~fit_model(formulas = .[[1]], analysis_frame = .[[2]],
                        cores = brm_cores, iter = n_markov_iter))
