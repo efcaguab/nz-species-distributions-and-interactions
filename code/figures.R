8d8a63a806164c9084cd3770b04b1f3a2aab9ae4
efcaguab <efcaguab@gmail.com>
2019-09-13 11:12:45 +1200

    modify pallete plants vs animals

--- code/figures.R
+++ code/figures.R
@@ -227,7 +227,7 @@ plot_ranf <- function(random_species_draws, random_correlation_posterior, random
     require(ggforce)
   })
 
-  pal <- cgm()$pal_el_green[c(8,7)]
+  pal <- cgm()$pal_el_green[c(8,6)]
 
   conditional_effects_plot <- random_species_draws %>%
     dplyr::ungroup() %>%
@@ -273,70 +273,41 @@ plot_ranf <- function(random_species_draws, random_correlation_posterior, random
     geom_density(fill = cgm()$pal_el_green[1], colour = NA) +
     stat_density(geom = "line", colour = cgm()$pal_el_green[9], size = 0.25) +
     geom_vline(xintercept = mean_correlation, size = 0.25, linetype = 2) +
-    coord_cartesian(expand = FALSE) +
+    coord_cartesian(expand = T) +
     pub_theme() +
     theme(panel.border = element_blank(),
           axis.title.y = element_blank(),
           axis.text.y = element_blank(),
           axis.ticks.y = element_blank(),
-          axis.line.x.bottom = element_line(),
-          plot.title = element_text(margin = margin(b = 4))) +
-    labs(title = "(c) correlation coefficient")
+          axis.line.x.bottom = element_line()) +
+    labs(title = "(b) correlation between random intercept and slope")
 
-  slope_intercept_plot <- random_slope_intercepts %>%
-    dplyr::left_join(random_sp_names, by = "org_id") %>%
-    dplyr::mutate(mark = !is.na(sp_name)) %>%
+  slope_intercept_plot <- random_species_draws %>%
+    dplyr::select(org_id, guild) %>%
+    dplyr::inner_join(random_slope_intercepts) %>%
     ggplot(mapping = aes(colour = guild,
-                         #fill = guild,
                          x = Intercept_Estimate,
                          y = scaled_suitability_Estimate)) +
+    geom_point(shape = 21, stroke = 0.25, size = 1) +
+    geom_errorbar(mapping = aes(ymin = scaled_suitability_Q25,
+                                ymax = scaled_suitability_Q75),
+                  size = 0.15, alpha = 0.25) +
+    geom_errorbarh(mapping = aes(xmin = Intercept_Q25,
+                                 xmax = Intercept_Q75),
+                   size = 0.15, alpha = 0.25) +
     geom_hline(yintercept = 0, linetype = 2, size = 0.25) +
     geom_vline(xintercept = 0, linetype = 2, size = 0.25) +
-    geom_point(shape = 21, stroke = 0.25, size = 1) +
-    #     geom_mark_circle(aes(group = org_id,
-    #                          filter = mark,
-    #                           label = sp_name),
-    #                      expand = unit(0, "mm"),
-    #                      alpha = 0.1,
-    #     concavity = 0,
-  #                       colour = "transparent",
-  #                       fill = "grey",
-  #                       label.fontsize = 7,
-  #                       label.buffer = unit(5, "mm"),
-  #                       label.minwidth = unit(10, "mm"),
-  #                       label.margin = margin(0.5, 0.5, 0.5, 0.5, "mm"),
-  #                       label.fontface = "italic",
-  #                       con.size = 0.25,
-  #                       con.cap = unit(0.1, "mm"),
-  #                       con.type = "straight") +
-    #     geom_errorbar(mapping = aes(ymin = scaled_suitability_Q25,
-    #                                 ymax = scaled_suitability_Q75),
-    #                   size = 0.15, alpha = 0.25) +
-    #     geom_errorbarh(mapping = aes(xmin = Intercept_Q25,
-    #                              xmax = Intercept_Q75),
-    #                size = 0.15, alpha = 0.25) +
-    scale_fill_manual(values = pal, aesthetics = c("fill", "colour")) +
-    coord_cartesian(expand = TRUE) +
-    pub_theme() +
-    theme(legend.position = "none") +
-    labs(x = "species' intercept",
-         y = "species' suitability slope",
-         title = "(b) species' specific intercept and suitability slope",
-         subtitle = "all values in parameter space")
-
+    pub_theme()
   plot_left <- conditional_effects_plot
-
   plot_right <- cowplot::plot_grid(slope_intercept_plot,
                                    correlation_plot,
                                    ncol = 1,
-                                   rel_heights = c(3, 1),
-                                   axis = "lr",
+                                   rel_heights = c(2, 1),
                                    align = "v")
-
   p <- cowplot::plot_grid(plot_left,
                           plot_right,
                           ncol = 2)
-  #  ggsave("plot.pdf", p, width = unit(width("double"), "in"), height = unit(2.2*2, "in"))
+  ggsave("plot.pdf", p, width = unit(width("double"), "in"), height = unit(2.2*2.5, "in"))
 
   p
 }
