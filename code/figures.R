c564495fb5875e180b8de33b4e9ad259b01e3705
Fernando Cagua <efcaguab@gmail.com>
2020-05-24 22:23:35 +1200

    Make Fig 1 a inverse cumulative distribution

--- code/figures.R
+++ code/figures.R
@@ -35,34 +35,38 @@ plot_species_location_distribution <- function(dist_species_multiple_locations_d
   pal <- cgm()$pal_el_green[c(4,7)]
 
    p <- dist_species_multiple_locations_data %>%
-    ggplot(aes(x = n_locations, y = n_species, fill = guild)) +
-    geom_line(aes(colour = guild), linetype = 3, size = 0.5) +
-    geom_point(shape = 21, size = 1) +
-    # geom_text_repel(data = distribution_data_highlight, aes(label = sp_name)) +
-    geom_mark_circle(aes(label = sp_name, filter = !is.na(sp_name), group = sp_name),
-                     expand = unit(0.1, "mm"),
-                     alpha = 0.1,
-                     colour = NA,
-                     label.fontsize = 7,
-                     label.buffer = unit(5, "mm"),
-                     label.minwidth = unit(10, "mm"),
-                     label.margin = margin(0.5, 0.5, 0.5, 0.5, "mm"),
-                     label.fontface = "italic",
-                     con.size = 0.25,
-                     con.cap = unit(1, "mm"),
-                     con.type = "straight") +
-    scale_fill_manual(values = pal, aesthetics = c("fill", "colour"),
-                      labels = c("animals", "plants")) +
-    scale_y_log10(labels = scales::number_format(big.mark = ",")) +
-    scale_x_log10(breaks = c(1,2, 4, 8, 16, 32)) +
-    pub_theme() +
-    theme(legend.position = c(0.95,0.95),
-          legend.justification = c(1,1),
-          legend.title = element_blank()) +
-    labs(x = "number of locations",
-         y = "frequency (# species)",
-         title = "number of species at multiple locations",
-         subtitle = "frequency distribution")
+     dplyr::arrange(guild, desc(n_locations)) %>%
+     dplyr::mutate(cn = cumsum(n_species), 
+                   p = cn / max(cn)) %>% 
+     ggplot(aes(n_locations, cn, fill = guild)) +
+     geom_line(aes(colour = guild), linetype = 3, size = 0.5) +
+     geom_point(shape = 21, size = 1) +
+     # geom_text_repel(data = distribution_data_highlight, aes(label = sp_name)) +
+     geom_mark_circle(aes(label = sp_name, filter = !is.na(sp_name), group = sp_name),
+                      expand = unit(0.1, "mm"),
+                      alpha = 0.1,
+                      colour = NA,
+                      label.fontsize = 7,
+                      label.buffer = unit(5, "mm"),
+                      label.minwidth = unit(10, "mm"),
+                      label.margin = margin(0.5, 0.5, 0.5, 0.5, "mm"),
+                      label.fontface = "italic",
+                      con.size = 0.25,
+                      con.cap = unit(1, "mm"),
+                      con.type = "straight") +
+     scale_fill_manual(values = pal, aesthetics = c("fill", "colour"),
+                       labels = c("animals", "plants")) +
+     scale_y_log10(labels = scales::number_format(big.mark = ",")) +
+     # scale_y_log10() +
+     scale_x_log10(breaks = c(1,2, 4, 8, 16, 32)) +
+     pub_theme() +
+     theme(legend.position = c(0.95,0.95),
+           legend.justification = c(1,1),
+           legend.title = element_blank()) +
+     labs(x = "number of locations",
+          y = "# species",
+          title = "number of species at multiple locations",
+          subtitle = "inverse cummulative distribution")
 
   # ggsave("plot.pdf", p,  width = unit(width("single"), "in"), height = unit(2.2, "in"))
    p
--- paper/abstract.md
+++ paper/abstract.md
@@ -2,7 +2,7 @@
 Evidence that the environment can influence species interactions is rapidly accumulating, but can it also influence whether a species is a generalist or a specialist? 
 To date, a systematic link between the environment and specialisation has been elusive, perhaps because different environmental variables produce contrasting effects on different species. 
 Here, we use a different approach and test whether there is a link between the *stresses* imposed by the environment and species specialisation. 
-Using a global dataset of plant-pollinator interactions, we found evidence that, even when accounting for changes in community composition, environmental stress can influence intermediate specialisation. 
+Using a global dataset of plant-pollinator interactions, we found evidence that, even when accounting for changes in community composition, environmental stress can indeed influence intermediate specialisation. 
 Under stressful environmental conditions, species that would otherwise interact with many partners tended to focus on fewer, presumably higher-quality, interactions. 
 In contrast, specialists that were present in multiple communities tended to broaden their niche to cope with increased environmental stress, presumably by engaging in more opportunistic interactions. 
 <!-- Facultative generalism tends to be the norm, rather than the exception, and therefore a species’ level of specialisation should usually be considered on a relative scale. -->
